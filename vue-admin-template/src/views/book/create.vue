f205771120051c84c1b88515ddd194dbf0296fb6
waney316 <1119739725@qq.com>
2021-04-25 09:31:16 +0800

    add create

--- vue-admin-template/src/views/book/create.vue
+++ vue-admin-template/src/views/book/create.vue
@@ -5,54 +5,42 @@
         <span>新增图书</span>
       </div>
       <div class="text item">
-       <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-  <el-form-item label="活动名称" prop="name">
-    <el-input v-model="ruleForm.name"></el-input>
-  </el-form-item>
-  <el-form-item label="活动区域" prop="region">
-    <el-select v-model="ruleForm.region" placeholder="请选择活动区域">
-      <el-option label="区域一" value="shanghai"></el-option>
-      <el-option label="区域二" value="beijing"></el-option>
-    </el-select>
-  </el-form-item>
-  <el-form-item label="活动时间" required>
-    <el-col :span="11">
-      <el-form-item prop="date1">
-        <el-date-picker type="date" placeholder="选择日期" v-model="ruleForm.date1" style="width: 100%;"></el-date-picker>
-      </el-form-item>
-    </el-col>
-    <el-col class="line" :span="2">-</el-col>
-    <el-col :span="11">
-      <el-form-item prop="date2">
-        <el-time-picker placeholder="选择时间" v-model="ruleForm.date2" style="width: 100%;"></el-time-picker>
-      </el-form-item>
-    </el-col>
-  </el-form-item>
-  <el-form-item label="即时配送" prop="delivery">
-    <el-switch v-model="ruleForm.delivery"></el-switch>
-  </el-form-item>
-  <el-form-item label="活动性质" prop="type">
-    <el-checkbox-group v-model="ruleForm.type">
-      <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-      <el-checkbox label="地推活动" name="type"></el-checkbox>
-      <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-      <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-    </el-checkbox-group>
-  </el-form-item>
-  <el-form-item label="特殊资源" prop="resource">
-    <el-radio-group v-model="ruleForm.resource">
-      <el-radio label="线上品牌商赞助"></el-radio>
-      <el-radio label="线下场地免费"></el-radio>
-    </el-radio-group>
-  </el-form-item>
-  <el-form-item label="活动形式" prop="desc">
-    <el-input type="textarea" v-model="ruleForm.desc"></el-input>
-  </el-form-item>
-  <el-form-item>
-    <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
-    <el-button @click="resetForm('ruleForm')">重置</el-button>
-  </el-form-item>
-</el-form>
+        <el-form
+          :model="ruleForm"
+          :rules="rules"
+          ref="ruleForm"
+          label-width="100px"
+          class="demo-ruleForm"
+        >
+
+          <el-form-item label="书名" prop="bookName">
+            <el-input v-model="ruleForm.bookName"></el-input>
+          </el-form-item>
+          <el-form-item label="作者" prop="author">
+            <el-input v-model="ruleForm.author"></el-input>
+          </el-form-item>
+          <el-form-item label="出版社" prop="publisher">
+            <el-input v-model="ruleForm.publisher"></el-input>
+          </el-form-item>
+          <el-form-item label="出版日期" prop="publish_date">
+            <el-date-picker v-model="ruleForm.publish_date" type="date" placeholder="选择日期">
+            </el-date-picker>
+          </el-form-item>
+
+          <el-form-item label="发行状态" prop="publish_status">
+            <el-radio-group v-model="ruleForm.publish_status">
+              <el-radio label="已发行"></el-radio>
+              <el-radio label="未发行"></el-radio>
+            </el-radio-group>
+          </el-form-item>
+ 
+          <el-form-item>
+            <el-button type="primary" @click="submitForm('ruleForm')"
+              >立即创建</el-button
+            >
+            <el-button @click="resetForm('ruleForm')">重置</el-button>
+          </el-form-item>
+        </el-form>
       </div>
     </el-card>
   </div>
@@ -60,62 +48,53 @@
 
 <script>
 export default {
-    data() {
-      return {
-        ruleForm: {
-          name: '',
-          region: '',
-          date1: '',
-          date2: '',
-          delivery: false,
-          type: [],
-          resource: '',
-          desc: ''
-        },
-        rules: {
-          name: [
-            { required: true, message: '请输入活动名称', trigger: 'blur' },
-            { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
-          ],
-          region: [
-            { required: true, message: '请选择活动区域', trigger: 'change' }
-          ],
-          date1: [
-            { type: 'date', required: true, message: '请选择日期', trigger: 'change' }
-          ],
-          date2: [
-            { type: 'date', required: true, message: '请选择时间', trigger: 'change' }
-          ],
-          type: [
-            { type: 'array', required: true, message: '请至少选择一个活动性质', trigger: 'change' }
-          ],
-          resource: [
-            { required: true, message: '请选择活动资源', trigger: 'change' }
-          ],
-          desc: [
-            { required: true, message: '请填写活动形式', trigger: 'blur' }
-          ]
-        }
-      };
-    },
-    methods: {
-      submitForm(formName) {
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            alert('submit!');
-          } else {
-            console.log('error submit!!');
-            return false;
-          }
-        });
+  data() {
+    return {
+      ruleForm: {
+        bookName: "",
+        author: "",
+        publisher: "",
+        publish_date: "",
+        publish_status: ""
       },
-      resetForm(formName) {
-        this.$refs[formName].resetFields();
-      }
-    }
-  }
+      rules: {
+        bookName: [
+          { required: true, message: "请输入图书名称", trigger: "blur" },
+        ],
+        author: [
+          { required: true, message: "请输入作者", trigger: "change" },
+        ],
+        publish_date: [
+          {
+            type: "date",
+            required: true,
+            message: "请选择日期",
+            trigger: "change",
+          },
+        ],
+        publisher: [{ required: true, message: "请输入出版社", trigger: "change" },],
+        publish_status: [{ required: true, message: "请选择出版状态", trigger: "change" }],
+      },
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          console.log(this.$refs[formName]);
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
