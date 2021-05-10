6b6ac9c92732882bd32b4a753a1640dd7a0e3159
waney316 <1119739725@qq.com>
2021-05-10 20:35:58 +0800

    fix  zabbixinfo

---
+++ cmdb/src/views/zabbix/test.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <el-button
+      type="success"
+      @click="handleConnect"
+    >测试连接</el-button>
+    <el-dialog :visible.sync="dialogInfoVisible">
+      用户ID: {{ info.userid }}
+      用户名：{{ info.username }}
+    </el-dialog>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      dialogInfoVisible: false,
+      info: ""
+    }
+  },
+  methods: {
+    handleConnect () {
+      const data = { name: "test" }
+      getList(data).then(response => {
+        // response_data = {"userid": 001, "username": "test", }
+        this.info = response_data
+        this.dialogInfoVisible = true
+      });
+    }
+  }
+}
+</script>
